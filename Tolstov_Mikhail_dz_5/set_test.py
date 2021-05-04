b0813dc51656493c198233b1b7616dff1988c0fa
Dreaminkv <inkver@gmail.com>
2021-05-04 11:24:03 +0300

    del dz 5 on master

--- Tolstov_Mikhail_dz_5/set_test.py
+++
@@ -1,8 +0,0 @@
-name_list = ['Basil', 'Kirill', 'Basil', 'Katya', 'Katya', 'Katya', 'Lena']
-print(name_list)
-
-name_set = set(name_list)
-print(name_set)
-name_set.add('Lena')
-name_set.add('ivan')
-print(name_set)
