da4016a02bef8c2fb116a321cb044dec154ea612
Dreaminkv <inkver@gmail.com>
2021-05-04 11:21:21 +0300

    add all task

---
+++ Tolstov_Mikhail_dz_5/task_5_1.py
@@ -0,0 +1,21 @@
+# 1. Написать генератор нечётных чисел от 1 до n (включительно),
+# используя ключевое слово yield, например:
+# >>> odd_to_15 = odd_nums(15)
+# >>> next(odd_to_15)
+# 1
+# >>> next(odd_to_15)
+# 3
+# ...
+# >>> next(odd_to_15)
+# 15
+# >>> next(odd_to_15)
+# ...StopIteration...
+def odd_nums(num):
+    for _ in range(0, num + 1):
+        if _ % 2 != 0:
+            yield _
+
+
+odd_to_15 = odd_nums(15)
+print(next(odd_to_15))
+print(next(odd_to_15))
---
+++ Tolstov_Mikhail_dz_5/task_5_4.py
@@ -0,0 +1,51 @@
+# 4. Представлен список чисел. Необходимо вывести те его элементы,
+# значения которых больше предыдущего, например:
+# src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+# result = [12, 44, 4, 10, 78, 123]
+#
+#
+# Подсказка: использовать возможности python, изученные на уроке.
+# Подумайте, как можно сделать оптимизацию кода по памяти, по скорости.
+import timeit
+import sys
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+
+max_src = [src[i] for i in range(1, len(src)) if src[i] > src[i-1]]
+print(max_src)
+
+
+def max_src(src):
+    result = []
+    for i in range(1, len(src)):
+        if src[i] > src[i - 1]:
+            result.append(src[i])
+    yield result
+
+
+print(*max_src(src))
+
+print('Затрачено пямяти с помощью List Comprehensions: ', sys.getsizeof(max_src))
+print('Затрачено пямяти с помощью генератора', sys.getsizeof(max_src(src)))
+
+
+
+code_to_test_1 = """
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+max_src = [src[i] for i in range(1, len(src)) if src[i] > src[i-1]]
+"""
+
+elapsed_time_1 = timeit.timeit(code_to_test_1, number=100)/100
+print('Время выполнения List Comprehensions: ', elapsed_time_1)
+
+code_to_test = """
+def max_src(src):
+    result = []
+    for i in range(1, len(src)):
+        if src[i] > src[i - 1]:
+            result.append(i)
+    yield result
+"""
+
+elapsed_time_2 = timeit.timeit(code_to_test, number=100)/100
+print('Время выполнения генератора: ', elapsed_time_2)
