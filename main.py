0344cac7ae2086b7ffb9701d8ab163783e40e4ac
tolstov.my <tolstov.my@yandex.ru>
2021-05-26 18:01:12 +0300

    add task 9_1

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+# class Auto:
+#     # атрибуты класса
+#     auto_count = 0
+#
+#     # методы класса
+#     def on_auto_start(self, auto_name, auto_model, auto_year):
+#         print("Автомобиль заведен")
+#         self.auto_name = auto_name
+#         self.auto_model = auto_model
+#         self.auto_year = auto_year
+#         self.auto_count += 1
+#
+# a = Auto()
+# a.on_auto_start('Mazda', 'Rx8', 2020)
+# print(a.auto_name)
+# print(a.auto_count)
+#
+# a3 = Auto()
+# a3.on_auto_start('Mazda', 'Rx8', 2020)
+# print(a3.auto_name)
+# print(a3.auto_count)
+#
+# a = Auto()
+# a.on_auto_start('Mazda', 'Rx8', 2020)
+# print(a.auto_name)
+# print(a.auto_count)
+class Player:
+    def player_method(self):
+        print("Родительский метод класса Player")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class Navigator:
+    def navigator_method(self):
+        print("Родительский метод класса Navigator")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MobilePhone(Player, Navigator):
+    def mobile_phone_method(self):
+        print("Дочерний метод класса MobilePhone")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+m_p = MobilePhone()
+m_p.player_method()
+m_p.navigator_method()
